3c708f4abed01bdd0475886705dd1a02227d7dd7
Vladislav07 <belov_vlad@list.ru>
2025-07-25 14:47:42 +0300

    25

--- AddInPageMate/AddInPageMate/SolidServise.cs
+++ AddInPageMate/AddInPageMate/SolidServise.cs
@@ -87,6 +87,18 @@ namespace AddInPageMate
                 string nChild = compChild.Name2 + "@" + nameAssemble;         
                 MathTransform compInNewSKR = (MathTransform)swTrChild.Multiply(MtrInvPlaneBase.Inverse());
 
+                double[] arrayMatrixComp = (double[])compInNewSKR.ArrayData;
+                List<LocationComponent> components;
+                bool res = DetermineAngleAndPosition(arrayMatrixComp, out components);
+                
+                if (res) {
+                    DeletingMateComp(compChild);
+                    AddMate(components);
+                    components=null;
+                    return true;
+                }
+                    
+
                 double ScaleOutb = 0;
                 Object Xch = null;
                 Object Ych = null;
@@ -99,9 +111,9 @@ namespace AddInPageMate
                 listVecor[0] = (MathVector)Xch;
                 listVecor[1] = (MathVector)Ych;
                 listVecor[2] = (MathVector)Zch;
-
+                
                 double[] coord = (double[])((MathVector)TrObjOutch).ArrayData;
-    
+                
                 for (int i = 0; i <3; i++)
                 {
                     LocationComponent l = new LocationComponent();
@@ -330,8 +342,111 @@ namespace AddInPageMate
             angleComp.Fliped = !direction;
      
         }
+        public static bool DetermineAngleAndPosition(double[] arrMatrix, out List<LocationComponent> locationComp)
+        {
+            List<double> list = arrMatrix.Take(9).ToList();
+            list.ForEach(n => Math.Round(n, 3));
+            locationComp = null;
+            if (list.Any(n=>n==0))return false;
+            double[,] resultArray = new double[3, 3];
+      
+            int index = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultArray[i, j] = list[index];
+                    index++;
+                }
+            }
+            locationComp = CalculateAnglesAndAlignmentWithGlobalPlanes(resultArray);
 
-       
+            return true;
+        }
+
+        private static LocationComponent GetLocationAngleComp(double angle,bool flip, bool direction, string plane)
+        {
+         
+            LocationComponent locationComp = new LocationComponent();
+            locationComp.Fliped=flip;
+            locationComp.PlanComp = plane;
+            locationComp.childComp=plane;
+            locationComp.TypeSelectBase = "PLANE";
+            locationComp.TypeSelect = "PLANE";
+            locationComp.dist = 0;
+            locationComp.mateType = swMateType_e.swMateANGLE;
+            locationComp.Align = direction? swMateAlign_e.swMateAlignALIGNED: swMateAlign_e.swMateAlignANTI_ALIGNED;
+            locationComp.Angle = angle;
+            return locationComp;
+        }
+
+        private static List<LocationComponent> CalculateAnglesAndAlignmentWithGlobalPlanes(double[,] localTransformationMatrix)
+        {
+            List<LocationComponent> components = new List<LocationComponent>();
+            // Нормали к плоскостям глобальной системы координат
+            double[] globalNormalYZ = { 1, 0, 0 }; // Нормаль к плоскости YZ
+            double[] globalNormalXZ = { 0, 1, 0 }; // Нормаль к плоскости XZ
+            double[] globalNormalXY = { 0, 0, 1 }; // Нормаль к плоскости XY
+
+            // Нормали плоскостей локального компонента из матрицы преобразования
+            double[] localNormalX = { localTransformationMatrix[0, 0], localTransformationMatrix[1, 0], localTransformationMatrix[2, 0] };
+            double[] localNormalY = { localTransformationMatrix[0, 1], localTransformationMatrix[1, 1], localTransformationMatrix[2, 1] };
+            double[] localNormalZ = { localTransformationMatrix[0, 2], localTransformationMatrix[1, 2], localTransformationMatrix[2, 2] };
+
+            // Вычисляем углы между плоскостями локального компонента и глобальными плоскостями
+            double angleToYZ = CalculateAngleBetweenPlanes(localNormalX, globalNormalYZ);
+            double angleToXZ = CalculateAngleBetweenPlanes(localNormalY, globalNormalXZ);
+            double angleToXY = CalculateAngleBetweenPlanes(localNormalZ, globalNormalXY);
+
+            // Определяем сонаправленность нормалей
+            bool alignedWithYZ = AreNormalsAligned(localNormalX, globalNormalYZ);
+            bool alignedWithXZ = AreNormalsAligned(localNormalY, globalNormalXZ);
+            bool alignedWithXY = AreNormalsAligned(localNormalZ, globalNormalXY);
+
+            bool isFlipedX = CalculateAngleDirectionWithGlobalPlanes(localNormalX, globalNormalYZ);
+            bool isFlipedY = CalculateAngleDirectionWithGlobalPlanes(localNormalY, globalNormalXZ);
+            bool isFlipedZ = CalculateAngleDirectionWithGlobalPlanes(localNormalZ, globalNormalXY);
+
+            LocationComponent locationCompX = GetLocationAngleComp(angleToYZ, isFlipedX, alignedWithYZ, "Rigth");
+            components.Add(locationCompX);               
+            LocationComponent locationCompY = GetLocationAngleComp(angleToXZ, isFlipedY, alignedWithXZ, "Top");
+            components.Add(locationCompY);               
+            LocationComponent locationCompZ = GetLocationAngleComp(angleToXY, isFlipedZ, alignedWithXY, "Front");
+            components.Add(locationCompZ);
+
+            return components;
+        }
+        private static double CalculateAngleBetweenPlanes(double[] normal1, double[] normal2)
+        {
+            double dotProduct = normal1[0] * normal2[0] + normal1[1] * normal2[1] + normal1[2] * normal2[2];
+            double magnitude1 = Math.Sqrt(normal1[0] * normal1[0] + normal1[1] * normal1[1] + normal1[2] * normal1[2]);
+            double magnitude2 = Math.Sqrt(normal2[0] * normal2[0] + normal2[1] * normal2[1] + normal2[2] * normal2[2]);
+            double cosAngle = dotProduct / (magnitude1 * magnitude2);
+            double angle = Math.Acos(cosAngle) * 180 / Math.PI; // Преобразуем угол из радиан в градусы
+
+            return angle;
+        }
+
+        private static bool AreNormalsAligned(double[] normal1, double[] normal2)
+        {
+            double dotProduct = normal1[0] * normal2[0] + normal1[1] * normal2[1] + normal1[2] * normal2[2];
+            return dotProduct > 0;
+        }
+        public static bool CalculateAngleDirectionWithGlobalPlanes(double[] localNormal, double[] globalNormal)
+        {
+            // Векторное произведение нормалей плоскостей
+            double[] crossProduct = CrossProduct(localNormal, globalNormal);
+            // Определяем направление угла
+            return crossProduct[2] > 0 ? true : false;         //"по часовой стрелке" : "против часовой стрелки"; 
+        }
+
+        private static double[] CrossProduct(double[] vector1, double[] vector2)
+        {
+            double x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+            double y = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+            double z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+            return new double[] { x, y, z };
+        }
 
     }
 }
